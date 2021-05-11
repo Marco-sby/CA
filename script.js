f7bcd2607d860ca1b5f89bed06e626911a6392ed
marco dos santos <marco.santt@outlook.com>
2021-05-11 12:59:49 +0100

    Password test

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function getData(){
             author.forEach(function (lists){
                 output +=`
                     <div class="container">
-                            <div class="card mt-2 bg-light">
+                            <div class="card mt-1 bg-light">
                                 <ul class="list- group">
                                     <li class="list-group-item"><h2>Name: ${lists.name.first}</h2></li>
                                     <li class="list-group-item"><image src = ${lists.picture.large}></image></li>
@@ -37,5 +37,53 @@ function getData(){
             });
             document.getElementById('output').innerHTML = output;
        });
-
 };
+        function _id(name){
+            return document.getElementById(name);
+        }
+        function _class(name){
+            return document.getElementsByClassName(name);
+        }
+        _class("toggle-password")[0].addEventListener("click",function(){
+            _class("toggle-password")[0].classList.toggle("active");
+            if(_id("password-field").getAttribute("type") == "password"){
+            _id("password-field").setAttribute("type","text");
+            } else {
+            _id("password-field").setAttribute("type","password");
+            }
+        });
+        
+        _id("password-field").addEventListener("focus",function(){
+            _class("password-policies")[0].classList.add("active");
+        });
+        _id("password-field").addEventListener("blur",function(){
+            _class("password-policies")[0].classList.remove("active");
+        });
+        
+        _id("password-field").addEventListener("keyup",function(){
+            let password = _id("password-field").value;
+            
+            if(/[A-Z]/.test(password)){
+            _class("policy-uppercase")[0].classList.add("active");
+            } else {
+            _class("policy-uppercase")[0].classList.remove("active");
+            }
+            
+            if(/[0-9]/.test(password)){
+            _class("policy-number")[0].classList.add("active");
+            } else {
+            _class("policy-number")[0].classList.remove("active");
+            }
+            
+            if(/[^A-Za-z0-9]/.test(password)){
+            _class("policy-special")[0].classList.add("active");
+            } else {
+            _class("policy-special")[0].classList.remove("active");
+            }
+            
+            if(password.length > 7){
+            _class("policy-length")[0].classList.add("active");
+            } else {
+            _class("policy-length")[0].classList.remove("active");
+            }
+        });
